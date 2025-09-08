59d1893f608323ce1a83b971cba46580807d5403
sandeepshahi <s.sandeepshahi@gmail.com>
2025-09-08 12:47:51 +0530

    Add python-compiler generator | Add pyspark-jobs

---
+++ py-validation-generator/index.ts
@@ -0,0 +1,28 @@
+import { ConfigCompiler } from "ondc-code-generator";
+import { SupportedLanguages } from "ondc-code-generator/dist/types/compiler-types";
+import path from "path";
+import { readFileSync } from "fs";
+import { loadAndDereferenceYaml } from "./utils/yaml-utils.js";
+
+const runCompiler = async (buildYaml: any, outputPath: string) => {
+  const comp = new ConfigCompiler(SupportedLanguages.Python);
+  const buildString = buildYaml;
+  const buildParsed = (await loadAndDereferenceYaml(buildString)) as any;
+  const valParsed = buildParsed["x-validations"];
+  await comp.initialize(buildString);
+  //   const x_validations = JSON.parse(valParsed) as any;
+  await comp.generateCode(
+    valParsed as any,
+    "l1_validations",
+    false,
+    outputPath
+  ); // pass the validations object and the name of the function of the generated code
+};
+
+let domain = "ret10";
+const buildYaml = readFileSync(
+  path.join(process.cwd(), `./build-yaml/${domain}/build.yaml`),
+  "utf8"
+);
+const outputPath = path.join(process.cwd(), `../validations/${domain}/`);
+runCompiler(buildYaml, outputPath);
